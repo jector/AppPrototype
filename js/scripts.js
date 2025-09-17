4b304fbbe746ba213316365354ee26ca488050ae
jector <jectorv@gmail.com>
2025-09-17 16:21:31 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function (){
+
+    // This is an example script, please modify as needed
+    const rangeInput = document.getElementById('range4');
+    const rangeOutput = document.getElementById('rangeValue');
+
+    // Set initial value
+    rangeOutput.textContent = rangeInput.value;
+
+    rangeInput.addEventListener('input', function() {
+        rangeOutput.textContent = this.value;
+    });
+
+
+
+
+});
+
+
+
